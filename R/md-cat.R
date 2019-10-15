113b4d5254d0ae6b322b4abc552ccb6fbd850a96
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-15 17:03:58 -0400

    Create internal if_cat() to check for cat

--- R/md-cat.R
+++ R/md-cat.R
@@ -6,3 +6,16 @@
 md_cat <- function(x) {
   cat(x, sep = "\n")
 }
+
+#' @title Output if Desired
+#' @param x The wrapped vector from a parent function.
+#' @param cat The `cat` argument from a parent function.
+#' @return A single character vector formatted for printing.
+if_cat <- function(x, cat) {
+  if (cat) {
+    md_cat(x)
+  } else {
+    return(x)
+  }
+}
+

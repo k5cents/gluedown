e93b7864b0d7f837b3c370e7404d9e38803edaa7
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-15 17:05:54 -0400

    Remove example from md_cat()

--- R/md-cat.R
+++ R/md-cat.R
@@ -1,8 +1,6 @@
 #' @title Output Separated Markdown
 #' @param x The markdown text to concatenate and separate with newlines.
 #' @return A single character vector formatted for printing.
-#' @examples
-#' md_cat(md_list(c("one", "two")))
 md_cat <- function(x) {
   cat(x, sep = "\n")
 }

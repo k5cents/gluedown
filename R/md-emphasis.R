a4cb798d7e85e276a80620b68cd6fade8bb3a922
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-15 20:29:36 -0400

    Create md_emphasize() to emphasize single word

--- R/md-emphasis.R
+++ R/md-emphasis.R
@@ -45,3 +45,25 @@ md_code <- function(x) {
 md_strike <- function(x) {
   glue::glue("~~{x}~~")
 }
+
+#' @title Markdown Selective Emphasize
+#' @param x A vectror of sentence character strings.
+#' @param f An emphasizing function (e.g., [md_bold()]).
+#' @param n The numeric _position_ of the word in each sentence to emphasize.
+#' @return A character vector with elements wrapped in two tilde symbols.
+#' @examples
+#' md_emphasize(x = "Live free or die", f = md_bold, n = 4)
+#' @importFrom stringr str_c str_split
+#' @importFrom purrr map_at
+#' @export
+md_emphasize <- function(x, f, n) {
+  # hacky solution
+  emphasize_at <- function(.x, .at, .f) {
+    stringr::str_c(purrr::map_at(.x, .at, .f), collapse = " ")
+  }
+  s <- stringr::str_split(x, pattern = " ")
+  for (i in seq_along(s)) {
+    s[[i]] <- emphasize_at(s[[i]], n, f)
+  }
+  unlist(s)
+}

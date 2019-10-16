2c6feda0b4c80ebade639436624cfe0b1540c9a6
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-15 22:09:48 -0400

    Create simple package documentation

---
+++ R/gluedown.R
@@ -0,0 +1,10 @@
+#' gluedown: A package to format character vectors with markdown.
+#'
+#' The gluedown package provides functions to create both container blocks and
+#' inlines from regular R character vectors. The package also helps uses these
+#' new formatted vectors as actual text in a markdown document.
+#'
+#'
+#' @docType package
+#' @name gluedown
+NULL

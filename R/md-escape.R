37a3c3ee6afa95d59c50ee5b9a944e2cf8c63ca5
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-15 21:56:30 -0400

    Create md_escape() helper function

---
+++ R/md-escape.R
@@ -0,0 +1,22 @@
+#' @title Markdown Escape Punctuation
+#' @description When trying to format text containing markdown syntax
+#'   characters, it's neccesary to "escape" those characters so that they are
+#'   ignored by formatting.
+#' @details From the [GFM spec](https://github.github.com/gfm/#backslash-escapes):
+#'  Any ASCII punctuation character may be backslash-escaped... Any ASCII
+#'  punctuation character may be backslash-escaped... Escaped characters are
+#'  treated as regular characters and do not have their usual Markdown meanings.
+#' @param x The text with punctuation to escape.
+#' @return A character string with all `[:punct:]` properly escaped with
+#'   prepended backslashes.
+#' @examples
+#' md_bold(md_escape("Use an * symbol in globs!"))
+#' @importFrom string str_replace_all
+#' @export
+md_escape <- function(x) {
+  stringr::str_replace_all(
+    string = x,
+    pattern = "([[:punct:]])",
+    replacement = "\\\\\\1"
+  )
+}

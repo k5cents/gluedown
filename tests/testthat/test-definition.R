a80a4566aa693b6f8dbc1f835650bdf727fba654
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-23 16:14:14 -0400

    Test md_define()
    Progress towards #5, but with knitr::pandoc() instead of md_convert(), which doesn't seem to work with md_define()

---
+++ tests/testthat/test-definition.R
@@ -0,0 +1,28 @@
+library(testthat)
+library(gluedown)
+library(knitr)
+library(rvest)
+
+test_that("multiplication works", {
+  input <- "test.md"
+  term <-"term"
+  def <- "definition"
+  file.create(input)
+  sink(input)
+  md_define(term, def, cat = TRUE)
+  sink()
+  file.show("test.md")
+  output <- pandoc(input, "html")
+  html <- read_html(output)
+  unlink(input)
+  unlink(output)
+  read_term <- html %>%
+    html_node("dl") %>%
+    html_node("dt") %>%
+    html_text(trim = TRUE)
+  read_def <- html %>%
+    html_node("dl") %>%
+    html_node("dd") %>%
+    html_text(trim = TRUE)
+  expect_true(object = all(term == read_term & def == read_def))
+})

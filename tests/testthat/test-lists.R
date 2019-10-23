7a534fae1ffa50339dc21256fa26cb7c6a930cd4
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-23 10:54:17 -0400

    Remove unnecessary seq list test

--- tests/testthat/test-lists.R
+++
@@ -1,9 +0,0 @@
-library(gluedown)
-library(stringr)
-
-test_that("sequential lists are possible", {
-  md_list(state.name, seq = TRUE) %>%
-    str_extract("^.+?(?=\\.)") %>%
-    as.numeric() -> x
-  expect_true(all(diff(x) == 1))
-})

b7f413909f6e3cc93ee72f203c50bc69ebfe153c
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-27 19:04:29 -0400

    Test md_link() titles and md_issue()

--- tests/testthat/test-links.R
+++ tests/testthat/test-links.R
@@ -1,6 +1,8 @@
 library(testthat)
 library(gluedown)
+library(stringr)
 library(rvest)
+library(httr)
 
 test_that("md_image make an string convertable to an HTML <img> tag", {
   node <- "man/figures/logo.png" %>%
@@ -21,6 +23,16 @@ test_that("md_link creates a string convertable to HTML href url", {
   expect_equal(url, node)
 })
 
+test_that("md_link creates a string with valid title", {
+  url <- "https://github.com/kiernann/gluedown"
+  node <- md_link("link", url, "gluedown") %>%
+    md_convert() %>%
+    read_html() %>%
+    html_node("a") %>%
+    html_attr("href")
+  expect_equal(url, node)
+})
+
 test_that("md_link creates a string convertable to HTML text", {
   text <- "link"
   node <- md_link(text, "https://github.com/kiernann/gluedown") %>%
@@ -31,10 +43,10 @@ test_that("md_link creates a string convertable to HTML text", {
   expect_equal(text, node)
 })
 
-test_that("", {
+test_that("md_reference creates a string convertable to HTML href url", {
   url <- "https://github.com/kiernann/gluedown"
-  text <- "link[link]"
-  ref <- md_reference("link", url)
+  text <- "link[1]"
+  ref <- md_reference("1", url)
   link <- str_c(text, ref, sep = "\n")
   node <- link %>%
     md_convert() %>%
@@ -42,4 +54,31 @@ test_that("", {
     html_node("a") %>%
     html_attr("href")
   expect_equal(url, node)
+})
+
+test_that("md_reference creates a string with valid title", {
+  url <- "https://github.com/kiernann/gluedown"
+  text <- "link[1]"
+  ref <- md_reference("1", url, "one")
+  link <- str_c(text, ref, sep = "\n")
+  node <- link %>%
+    md_convert() %>%
+    read_html() %>%
+    html_node("a") %>%
+    html_attr("href")
+  expect_equal(url, node)
+})
+
+test_that("md_issue creates GitHub auto-link URL", {
+  issue <- md_issue("kiernann/gluedown", 1)
+  convert <- str_c("https://github.com/", str_replace(issue, "#", "/issues/"))
+  url <- "https://github.com/kiernann/gluedown/issues/1"
+  expect_equal(convert, url)
+})
+
+test_that("md_issue creates a URL that exists", {
+  issue <- md_issue("kiernann/gluedown", 1)
+  convert <- str_c("https://github.com/", str_replace(issue, "#", "/issues/"))
+  status <- status_code(GET(convert))
+  expect_equal(status, 200)
 })

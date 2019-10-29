1fa129d42f1a7062f577cec77064ac12be134fb3
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-29 15:57:52 -0400

    Rename md-emphasis.R to md-inlines.R

--- R/md-inlines.R
+++ R/md-inlines.R
@@ -1,22 +1,6 @@
 #' @title Selective Markdown Emphasize
 #' @param x A vector of sentence character strings.
 #' @param n The numeric _position_ of the word in each sentence to emphasize.
-#' @param f An emphasizing function (e.g., [md_bold()]).
-#' @return A character vector with elements wrapped in two tilde symbols.
-#' @examples
-#' emphasize_at(x = "Live free or die", n = 4, f = md_bold)
-#' @importFrom stringr str_c str_split
-#' @importFrom purrr map_at
-#' @export
-emphasize_at <- function(x, f, n) {
-  s <- stringr::str_split(x, pattern = " ")
-  for (i in seq_along(s)) {
-    s2 <- purrr::map_at(.x = s[[i]], .at = n, .f = f)
-    s[[i]] <- stringr::str_c(s2, collapse = " ")
-  }
-  unlist(s)
-}
-
 #' @title Markdown Bold Emphasis
 #' @param x The text to be emphasized in bold.
 #' @return A character vector with elements wrapped in two asterisk symbols.

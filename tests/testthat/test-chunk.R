44014aa79b9ce0fccc6cf2f619e962825b8a0fda
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-29 12:58:45 -0400

    Test all of md_chunk()

--- tests/testthat/test-chunk.R
+++ tests/testthat/test-chunk.R
@@ -3,10 +3,10 @@ library(gluedown)
 library(stringr)
 library(rvest)
 
-test_that("md_chunk produces non-styled valid HTML fence blocks", {
+test_that("md_chunk produces valid HTML backtick fence blocks", {
   lines <- c("library(dplyr)", "starwars %>%", "filter(species == 'Droid')")
   read <- lines %>%
-    md_chunk() %>%
+    md_chunk(type = "tick") %>%
     md_convert() %>%
     read_html() %>%
     html_node("code") %>%
@@ -16,7 +16,20 @@ test_that("md_chunk produces non-styled valid HTML fence blocks", {
   expect_equal(read, raw)
 })
 
-test_that("md_chunk produces non-styled valid HTML ident blocks", {
+test_that("md_chunk produces non-styled valid HTML tilde fence blocks", {
+  lines <- c("library(dplyr)", "starwars %>%", "filter(species == 'Droid')")
+  read <- lines %>%
+    md_chunk(type = "tilde") %>%
+    md_convert() %>%
+    read_html() %>%
+    html_node("code") %>%
+    html_text(trim = TRUE)
+
+  raw <- paste(lines, collapse = "\n")
+  expect_equal(read, raw)
+})
+
+test_that("md_chunk produces valid HTML indented blocks", {
   lines <- c("library(dplyr)", "starwars %>%", "filter(species == 'Droid')")
   read <- lines %>%
     md_chunk(type = "indent") %>%
@@ -29,15 +42,6 @@ test_that("md_chunk produces non-styled valid HTML ident blocks", {
   expect_equal(read, raw)
 })
 
-test_that("md_chunk produces non-styled valid HTML tilde blocks", {
-  lines <- c("library(dplyr)", "starwars %>%", "filter(species == 'Droid')")
-  read <- lines %>%
-    md_chunk(type = "tilde") %>%
-    md_convert() %>%
-    read_html() %>%
-    html_node("code") %>%
-    html_text(trim = TRUE)
-
-  raw <- paste(lines, collapse = "\n")
-  expect_equal(read, raw)
+test_that("md_indent errors if less than 4 spaces are uses", {
+  expect_error(md_indent("code", n = 2))
 })

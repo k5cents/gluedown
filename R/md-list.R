141e597ed0bbda4e54bf1c22ace199839ed05c2d
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-29 16:02:02 -0400

    Create md_list() to call md_order(), etc

--- R/md-list.R
+++ R/md-list.R
@@ -1,25 +1,71 @@
-#' @title Markdown Bullet List
-#' @description Form a single-level unordered list from a vector.
-#' @details From the [GFM spec](https://github.github.com/gfm/#bullet-list-marker):
-#'   A bullet list marker is a `-`, `+`, or `*` character.
+#' @title Markdown list (5.4)
+#' @description Turn a character vector into a valid markdown list. The elements
+#'   creating this container block can then be redered as `<li>` HTML tags
+#'   inside `<ul>` or `<ol>` depending on the `type` of list desired. This is a
+#'   generic vector that calls [md_bullet()], [md_order()], or [md_task()]
+#'   depending on what string is provided in the `type` argument.
+#' @param x A chracter vector of list items.
+#' @param type The type of list to create; either bullet, ordered, or task.
+#' @param ... Arguments passed to the appropriate list type function.
+#' @return A `glue` vector with length equal to `x`.
+#' @family container block functions
+#' @examples
+#' md_list(state.name[1:5], type = "bullet", marker = "+")
+#' md_list(state.name[6:10], type = "ordered", marker = ")")
+#' md_list(state.name[11:15], type = "task", check = 3:5)
+#' @export
+md_list <- function(x, type = c("bullet", "ordered", "task"), ...) {
+  type <- match.arg(type)
+  if (type == "bullet") {
+    md_bullet(x, ...)
+  } else {
+    if (type == "ordered") {
+      md_order(x, ...)
+    } else {
+      md_task(x, ...)
+    }
+  }
+}
+
+#' @title Markdown bullet list (5.2)
+#' @description Turn a character vector into a valid markdown bullet list. This
+#'   container block can then be redered as nested `<li>` and `<ul>` HTML tags.
+#' @details A list is a sequence of one or more list items of the same type. The
+#'   list items may be separated by any number of blank lines.
+#'
+#'   Two list items are of the same type if they begin with a list marker of the
+#'   same type. Two list markers are of the same type if (a) they are bullet
+#'   list markers using the same character (`-`, `+`, or `*`)...
 #' @param x The vector of bullet point list items.
-#' @param marker The bullet list marker to use.
-#' @return A character vector with elements preceded by an asterisk symbol.
+#' @param marker The bullet list marker to use; one of `-`, `+`, or `*`.
+#' @return A `glue` vector with length equal to `x`.
 #' @family container block functions
 #' @examples
 #' md_bullet(state.name[1:5])
-#' md_bullet(state.name[1:5])
+#' md_bullet(sample(state.name, 5), marker = "+")
+#' @importFrom glue glue
 #' @export
 md_bullet <- function(x, marker = c("*", "-", "+")) {
   marker <- match.arg(marker)
   glue::glue("{marker} {x}")
 }
 
-#' @title Markdown Numbered List
-#' @description Form a single-level ordered list from a vector.
-#' @details From the [GFM spec](https://github.github.com/gfm/#ordered-list-marker):
-#'   An ordered list marker is a sequence of 1–9 arabic digits (`0-9`), followed
-#'   by either a `.` character or a `)` character.
+#' @title Markdown ordered list (5.2)
+#' @description Turn a character vector into a valid markdown ordered list. This
+#'   container block can then be redered as nested `<li>` and `<ol>` HTML tags.
+#' @details A list is a sequence of one or more list items of the same type. The
+#'   list items may be separated by any number of blank lines.
+#'
+#'   Two list items are of the same type if they begin with a list marker of the
+#'   same type. Two list markers are of the same type if (b) they are ordered
+#'   list numbers with the same delimiter (either `.` or `)`).
+#'
+#'   A list is an ordered list if its constituent list items begin with ordered
+#'   list markers, and a bullet list if its constituent list items begin with
+#'   bullet list markers.
+#'
+#'   The start number of an ordered list is determined by the list number of its
+#'   initial list item. The numbers of subsequent list items are disregarded.
 #' @param x The vector of numbered list items.
 #' @param seq logical; Should sequential numbers be used? Defaults to `TRUE`. If
 #'   `FALSE`, each element will be preceded by the number one; many markdown
@@ -27,19 +73,19 @@ md_bullet <- function(x, marker = c("*", "-", "+")) {
 #' @param pad logical; If sequential numbers are used, should they be padded
 #'   with zeroes on the left to match the width of the greatest number? Defaults
 #'   to `TRUE`.
-#' @param punct The punctuation mark following each number; either `.` or `)`.
-#' @return A character vector with elements preceded by a number.
+#' @param marker The ordered list marker following each arabic digits; either
+#'   `.` or `)`.
+#' @return A `glue` vector with length equal to `x`.
 #' @family container block functions
 #' @examples
-#' md_list(state.name[1:5])
-#' md_list(state.name[1:5])
+#' md_order(state.name[1:5])
+#' md_order(sample(state.name, 5), marker = ")")
+#' md_order(sample(state.name, 5), seq = FALSE)
 #' @importFrom stringr str_pad
+#' @importFrom glue glue
 #' @export
-md_list <- function(x, seq = TRUE, pad = TRUE, punct = c(".", ")")) {
-  punct <- match.arg(punct)
-  if (nchar(length(x)) > 9) {
-    stop("A maximum of 9 digits can be used as an unordered list marker. Your vector is too long.")
-  }
+md_order <- function(x, marker = c(".", ")"), seq = TRUE, pad = TRUE) {
+  marker <- match.arg(marker)
   if (seq) {
     nums <- seq_along(x)
     if (pad) {
@@ -53,30 +99,44 @@ md_list <- function(x, seq = TRUE, pad = TRUE, punct = c(".", ")")) {
   } else {
     nums <- 1
   }
-  glue::glue("{nums}{punct} {x}")
+  glue::glue("{nums}{marker} {x}")
 }
 
-#' @title Markdown Task List
-#' @description Create a markdown task list from a vector of tasks. Define which
-#'   task items should be checked off. Task lists are a GitHub specific markdown
-#'   feature.
-#' @details From the [GFM spec](https://github.github.com/gfm/#task-list-item):
-#'   A task [list item](https://github.github.com/gfm/#list-items) is a list
-#'   item where the first block in it is a paragraph which begins with a
-#'   [task list item marker](https://github.github.com/gfm/#task-list-item-marker)
-#'   and at least one whitespace character before any other content… A task list
-#'   item marker consists of an optional number of spaces, a left bracket (`[`),
-#'   either a whitespace character or the letter `x` in either lowercase or
-#'   uppercase, and then a right bracket (`]`)… When rendered, the task list
-#'   item marker is replaced with a semantic checkbox element… If the character
-#'   between the brackets is a whitespace character, the checkbox is unchecked.
-#'   Otherwise, the checkbox is checked.
-#' @param x The vector of task list items.
-#' @param check A vector of list elements which should be checked off.
-#' @return A character vector check box brackets prepended to each element.
+#' @title Markdown task list (5.3 extension)
+#' @description Turn a character vector into a valid markdown ordered list. This
+#'   container block can then be redered as nested `<li>` and `<ol>` HTML tags.
+#'   On venues supporting GitHub Flavored Markdown, this list will be specially
+#'   rendered with the list item market replaces with a
+#'   `<input type="checkbox">` HTML tag.
+#' @details GFM enables the tasklist extension, where an additional processing
+#'   step is performed on list items.
+
+#'   A task list item is a list item where the first block in it is a paragraph
+#'   which begins with a task list item marker and at least one whitespace
+#'   character before any other content.
+#'
+#'   A task list item marker consists of an optional number of spaces, a left
+#'   bracket (`[`), either a whitespace character or the letter `x` in either
+#'   lowercase or uppercase, and then a right bracket (`]`).
+#'
+#'   When rendered, the task list item marker is replaced with a semantic
+#'   checkbox element; in an HTML output, this would be an `<input
+#'   type="checkbox">` element.
+#'
+#'   If the character between the brackets is a whitespace character, the
+#'   checkbox is unchecked. Otherwise, the checkbox is checked.
+#'
+#'   This spec does not define how the checkbox elements are interacted with: in
+#'   practice, implementors are free to render the checkboxes as disabled or
+#'   inmutable elements, or they may dynamically handle dynamic interactions
+#'   (i.e. checking, unchecking) in the final rendered document.
+#' @param x A character vector of task list items.
+#' @param check A optional numeric vector of list elements which should be
+#'   checked off.
+#' @return A `glue` vector with length equal to `x`.
 #' @family container block functions
 #' @examples
-#' md_task(c("Wake up", "Eat Breakfast", "Brush Teeth"), check = 1:2)
+#' md_task(c("Wake up", "Eat Breakfast", "Brush Teeth"), check = c(1, 3))
 #' @importFrom glue glue
 #' @export
 md_task <- function(x, check = NULL) {

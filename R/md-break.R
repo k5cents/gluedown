8a856e5451da6c7ce93ff8288d7222c02f431273
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-29 10:27:02 -0400

    Improve docs and use str_dupe() in md_rule()

--- R/md-break.R
+++ R/md-break.R
@@ -1,21 +1,29 @@
-#' @title Markdown Thematic Break
+#' @title Markdown horizontal rule
+#' @description Print the markdown characters used to represent a _thematic
+#'   break_. This markdown leaf block can be rendered as the `<hr>` HTML tag.
 #' @details From the [GFM spec](https://github.github.com/gfm/): A line
 #'   consisting of 0-3 spaces of indentation, followed by a sequence of three or
 #'   more matching `-`, `_`, or `*` characters, each followed optionally by any
 #'   number of spaces or tabs, forms a thematic break.
 #' @param char The character used. Either an asterisk, hyphen, or underscore.
 #' @param n The number of times to repeat each character.
-#' @return A character vector of a repeated horizontal rule character.
+#' @return A repeated-character `glue` vector with length 1.
 #' @family leaf block functions
 #' @examples
-#' md_break("_")
-#' md_break(n = 10)
+#' md_rule()
+#' md_rule("_")
+#' md_rule(n = 10)
 #' @importFrom glue glue_collapse
+#' @importFrom stringr str_dup
 #' @export
-md_break <- function(char = c("*", "-", "_"), n = 3) {
-  if (n < 3)  { stop("At least 3 characters must be used") }
-  if (n > 80) { stop("Refrain from using more than 80 characters") }
+md_rule <- function(char = c("*", "-", "_"), n = 3) {
+  if (n < 3)  {
+    stop("At least 3 characters must be used")
+  }
+  if (n > 80) {
+    stop("Refrain from using more than 80 characters")
+  }
   char <- match.arg(char)
-  rule <- glue::glue_collapse(rep(char, n))
-  glue::glue("\n{rule}\n")
+  glue::glue("\n{stringr::str_dup(char, n)}\n")
 }
+

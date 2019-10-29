d195861a7fbfef515ae5a0f8191fa3f4d6c00b3c
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-29 11:33:07 -0400

    Split md_chunk() into fence and indent

--- R/md-chunk.R
+++ R/md-chunk.R
@@ -1,57 +1,104 @@
-#' @title Markdown Code Block
-#' @description Wrap a character vector (possibly lines of a file) in code
-#'   fences to be rendered as a code block.
+#' @title Markdown code block
+#' @description Turn a character vector of lines into a single code block either
+#'   indented or fenced in tildes or backticks. This markdown leaf block can be
+#'   rendered as nested HTML `<code>` and `<pre>` tags. This function either
+#'   calls [md_fence()] or [md_indent()] based on the `type` argument.
+#' @param x A character vector of lines to be wrapped concatenated into a
+#'   single block, possibly created by [readr::read_lines()] or some equivalent.
+#' @param ... Arguments to be passed to [md_fence()] or [md_indent()].
+#' @param type The type of code block to be created. Either "tick", "tilde"
+#'   (which call [md_fence()]) or "indent" (which calls [md_indent()]).
+#' @return A character vector wrapped on either side by code fences.
+#' @family leaf block functions
+#' @examples
+#' md_chunk("$ sudo apt install r-base-dev", lang = "bash")
+#' md_chunk(c("library(ggplot2)", "ggplot(mpg)+", "geom_point(aes(displ, hwy))"), type = "tilde")
+#' md_chunk(c("library(dplyr)", "starwars %>%", "filter(species == 'Droid')"), type = "indent", n = c(4, 4, 6))
+#' @export
+md_chunk <- function(x, type = c("tick", "tilde", "indent"), ...) {
+  type <- match.arg(type)
+  if (type == "tick") {
+    md_fence(x, char = "`", ...)
+  } else {
+    if (type == "tilde") {
+      md_fence(x, char = "~", ...)
+    } else {
+      md_indent(x, ...)
+    }
+  }
+}
+
+#' @title Markdown indented code block (4.4)
+#' @description Turn a character vector of lines into a single code block with
+#'   each line indented four spaces. This markdown leaf block can be rendered as
+#'   nested HTML `<code>` and `<pre>` tags. This is the code block format
+#'   required by legacy Reddit-flavored Markdown.
+#' @details From the [GFM spec](https://github.github.com/gfm/#indented-code-blocks):
+#'   An indented code block is composed of one or more indented chunks separated
+#'   by blank lines. An indented chunk is a sequence of non-blank lines, each
+#'   indented four or more spaces. The contents of the code block are the
+#'   literal contents of the lines, including trailing line endings, minus four
+#'   spaces of indentation. An indented code block has no info string.
+#' @param x A character vector of lines to be wrapped concatenated into a
+#'   single block, possibly created by [readr::read_lines()] or [deparse()].
+#' @param n A numeric vector
+#' @return A `glue` object of length 1, with the elements of `x` preceded with
+#'   4 spaces and separated by a newline.
+#' @family leaf block functions
+#' @examples
+#' md_indent(deparse(sd))
+#' md_indent(c("library(dplyr)", "starwars %>%", "filter(species == 'Droid')"), n = c(4, 4, 6))
+#' @importFrom glue glue glue_collapse
+#' @importFrom stringr str_dup
+#' @export
+md_indent <- function(x, n = 4) {
+  if (min(n) < 4) {
+    stop("Indented code blocks must be indented by at least four spaces.")
+  }
+  glue::glue_collapse(glue::glue("{stringr::str_dup(' ', n)}{x}"), sep = "\n")
+}
+
+#' @title Markdown fenced code block (4.5)
+#' @description Turn a character vector of lines into a single code block with
+#'   lines bookended with a code fence of backticks or tildes. This markdown
+#'   leaf block can be rendered as HTML `<code>` tags inside `<pre>` tags.
 #' @details From the [GFM spec](https://github.github.com/gfm/#code-fence): A
-#'   code fence is a sequence of at least three consecutive backtick characters…
-#'   A fenced code block begins with a code fence, indented no more than three
-#'   spaces… The line with the opening code fence may optionally contain some
-#'   text following the code fence… The content of the code block consists of
-#'   all subsequent lines, until a closing code fence… The content of a code
-#'   fence is treated as literal text, not parsed as inlines. The first word of
-#'   the info string is typically used to specify the language of the code
-#'   sample
-#' @param lines A character vector of lines to
-#'   be wrapped in fences, possibly created by [readr::read_lines()] or some
-#'   equivalent.
+#'   code fence is a sequence of at least three consecutive backtick characters
+#'   (`` ` ``) or tildes (`~`). (Tildes and backticks cannot be mixed.) A fenced
+#'   code block begins with a code fence, indented no more than three spaces.
+#'
+#'   The line with the opening code fence may optionally contain some text
+#'   following the code fence; this is trimmed of leading and trailing
+#'   whitespace and called the info string (see `lang`)...
+#'
+#'   The content of the code block consists of all subsequent lines, until a
+#'   closing code fence of the same type as the code block began with (backticks
+#'   or tildes), and with at least as many backticks or tildes as the opening
+#'   code fence...
+#'
+#'   The content of a code fence is treated as literal text, not parsed as
+#'   inlines. The first word of the info string is typically used to specify the
+#'   language of the code sample, and rendered in the class attribute of the
+#'   code tag.
+#' @param lines A character vector of lines to be wrapped concatenated into a
+#'   single block, possibly created by [readr::read_lines()] or some equivalent.
 #' @param lang The language of the code within the fence. Aside from clarity,
 #'   this allows many markdown engines to adjust the syntax highlighting.
 #'   Defaults to "r". This option is not used with indented blocks.
 #' @param type The type of code block to be created. Defaults to tipple
 #'   backticks code fences, but you can also use 4-spaces (useful for legacy
 #'   support on Reddit).
-#' @param style logical; If the `styler` package is installed, should
-#'   [styler::style_text()] be called on `lines`?
 #' @return A character vector wrapped on either side by code fences.
 #' @family leaf block functions
 #' @examples
-#' md_chunk(c("library(ggplot2)", "ggplot(mpg)+", "geom_point(aes(displ, hwy))"))
-#' md_chunk(c("library(dplyr)", "starwars %>%", "filter(species == 'Droid')"), type = "indent")
+#' md_fence(deparse(sd))
+#' md_fence(c("library(dplyr)", "starwars %>%", "  filter(species == 'Droid')"))
+#' @importFrom glue glue glue_collapse
+#' @importFrom stringr str_dup
 #' @export
-md_chunk <- function(lines, lang = "r", type = c("tick", "tilde", "indent"), style = TRUE) {
-  if (style & !requireNamespace("styler", quietly = FALSE)) {
-    stop("To style, the styler package needs to be installed with install.packages('styler')")
-  }
-  type <- match.arg(type)
-  fence <- if (type == "tick") {
-    "```"
-  } else {
-    "~~~"
-  }
-  if (type == "tick" | type == "tilde") {
-    string <- paste(lines, collapse = "\n")
-    if (style) {
-      styled <- paste(styler::style_text(string), collapse = "\n")
-      glue("{fence}{lang}\n{styled}\n{fence}")
-    } else {
-      glue("{fence}{lang}\n{string}\n{fence}")
-    }
-  } else {
-    if (style) {
-      string <- paste(lines, collapse = "\n")
-      styled <- styler::style_text(string)
-      glue("    {styled}")
-    } else {
-      glue("    {lines}")
-    }
-  }
+md_fence <- function(x, char = c("`", "~"), lang = "r") {
+  char <- match.arg(char)
+  string <- glue_collapse(x, sep = "\n")
+  fence <- stringr::str_dup(char, 3)
+  glue::glue("{fence}{lang}\n{string}\n{fence}")
 }

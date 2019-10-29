a66e144a6d3640ecf23e400932f1ad5ed5ababa2
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-29 10:31:34 -0400

    Improve docs and use str_dup() in md_heading()

--- R/md-heading.R
+++ R/md-heading.R
@@ -1,35 +1,37 @@
-#' @title Markdown ATX Headings
-#' @description Form a vector of markdown headings.
+#' @title Markdown ATX headings
+#' @description Turn a character vector into a vector of valid markdown ATX
+#'   headings. These markdown leaf blocks can be rendered as the `<h1>` through
+#'   `<h6>` HTML tags. See [md_setext()] to create setext (underlined) headings.
 #' @details From the [GFM spec](https://github.github.com/gfm/#atx-heading): An
 #'   ATX heading consists of a string of characters, parsed as inline content,
-#'   between an opening sequence of 1–6 unescaped `#` characters… The opening
+#'   between an opening sequence of 1–6 unescaped `#` characters... The opening
 #'   sequence of `#` characters must be followed by a space or by the end of
-#'   line… The heading level is equal to the number of `#` characters in the
+#'   line... The heading level is equal to the number of `#` characters in the
 #'   opening sequence.
-#' @param x The text of the headings.
-#' @param level The number of header hash characters. The heading level is equal
-#'   to the number of # characters in the opening sequence. In rendered markdown
-#'   higher levels are typically displayed larger.
-#' @return A character vector with elements preceded by hash symbols.
-#' @importFrom glue glue_collapse
+#' @param x A character vector of heading text.
+#' @param level A numeric vector of use to determine the number of heading hash
+#'   characters to preceed each element of `x`. The heading level is equal to
+#'   the number of `#` characters in the opening sequence.
+#' @return A `glue` vector of a length equal to `x`.
 #' @family leaf block functions
 #' @examples
 #' md_heading("Overview")
-#' md_heading(c("Installation", "Development version"), level = 1:2)
+#' md_heading(x = c("One", "Two"), level = 1:2)
+#' md_heading(x = c("Installation", "Usage"), level = 2)
+#' @importFrom glue glue
+#' @importFrom stringr str_dup
 #' @export
 md_heading <- function(x, level = 1) {
-  if (max(level) > 6) {
-    stop("More than 6 #'s is not a valid markdown heading.")
+  if (max(level) > 6 | min(level) < 1) {
+    stop("ATX headings only support levels 1 through 6.")
   }
-  hashes <- rep(NA, length(level))
-  for (i in seq_along(hashes)) {
-    hashes[i] <- glue::glue_collapse(rep("#", level[i]))
-  }
-  glue::glue("{hashes} {x}")
+  glue::glue("{stringr::str_dup('#', level)} {x}")
 }
 
-#' @title Markdown Setext Headings
-#' @description Form a single level 1 or 2 markdown setext headings.
+#' @title Markdown Setext headings
+#' @description Turn a character vector into a vector of valid markdown Setext
+#'   headings. These markdown leaf blocks can be rendered as the `<h1>` and
+#'   `<h2>` tags _only_.
 #' @details From the [GFM spec](https://github.github.com/gfm/#setext-headings):
 #'   A setext heading consists of one or more lines of text, each containing at
 #'   least one [non-whitespace character](https://github.github.com/gfm/#non-whitespace-character),
@@ -40,38 +42,25 @@ md_heading <- function(x, level = 1) {
 #'   trailing spaces... The heading is a level 1 heading if `=` characters are
 #'   used in the setext heading underline, and a level 2 heading if `-`
 #'   characters are used.
-#' @param x The text of the headings.
-#' @param level The heading level created; level 1 uses `=` and level 2 or
-#'   greater uses `-`.
-#' @return A heading with a setext underline the width of the longest line in `x`.
+#' @param x A character vector of heading text.
+#' @param level A numeric vector of use to determine the number of heading hash
+#'   characters to preceed each element of `x`. The heading level is equal to
+#'   the number of `#` characters in the opening sequence.
+#' @return A `glue` vector of a length equal to `x`.
 #' @importFrom glue glue glue_collapse
 #' @family leaf block functions
 #' @examples
 #' md_setext("Overview")
-#' md_setext("This is a setext\nHeading", level = 2)
-#' md_setext(c("one", "two", "three"), c(1, 2, 1))
+#' md_setext("This is a setext\nheading", level = 2)
+#' md_setext(c("one", "two", "three", "four"), level = c(1, 2))
 #' @export
 md_setext <- function(x, level = 1) {
-  if (length(x) != length(level)) {
-    stop("A level must be supplied for every element of x.")
+  char <- if (max(level) > 2 | min(level) < 1) {
+    stop("Setext headings only support levels 1 and 2.")
+  } else {
+    char <- c("=", "-")[level]
   }
-  char <- rep(NA, length(level))
-  for (i in seq_along(level)) {
-    char[i] <- if (level[i] > 2) {
-      warning("Setext headings do not support levels greater than 2.")
-      "-"
-    } else {
-      if (level[i] == 1) {
-        "="
-      } else {
-        "-"
-      }
-    }
-  }
+
   width <- sapply(strsplit(x, "\n"), function(y) max(nchar(y)))
-  lines <- rep(NA, length(x))
-  for (j in seq_along(x)) {
-    lines[j] <- glue::glue_collapse(rep(char[j], width[j]))
-  }
-  glue::glue("{x}\n{lines}")
+  glue::glue("{x}\n{stringr::str_dup(char, width)}")
 }

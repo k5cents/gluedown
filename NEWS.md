1affdb6ebbd320a529196087bb23876ae8a58887
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-30 19:31:30 -0400

    Increment patch version
    Check any() numeric num in md_issue(). This allows for vectorization of the num argument without the if() check throwing an error.

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# gluedown 0.2.2
+
+* Check if `any()` number in `md_issue()` can't be coerced to numeric.
+
 # gluedown 0.2.1
 
 * Return glued `md_convert()` output with `glue::as_glue()`.
--- R/md-issue.R
+++ R/md-issue.R
@@ -1,17 +1,19 @@
-#' @title Markdown Issue
-#' @description A markdown feature unique to GitHub. Allows users to hyperlink
-#' a specific issue or pull request using a username, repository name, and
+#' Markdown GitHub issue
+#'
+#' A markdown feature unique to GitHub. Allows users to hyperlink a specific
+#' issue or pull request using a "username/repository" combination and
 #' issue number.
+#'
 #' @param repo A character vector in the format `"user/rep"`.
 #' @param num The issue or pull number _without_ hash symbol.
 #' @return A character vector which GitHub can automatically hyperlink.
 #' @family inline functions
 #' @family markdown extensions
 #' @examples
-#' md_issue("kiernann/gluedown", 1)
+#' md_issue("kiernann/gluedown", 1:5)
 #' @export
 md_issue <- function(repo, num) {
-  if (is.na(as.numeric(num))) {
+  if (any(is.na(as.numeric(num)))) {
     stop("The num must be coercible to numeric.")
   }
   if (!grepl("/", repo)) {

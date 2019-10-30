d119e13b33b698583975666cc92458723caa41a8
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-30 10:10:30 -0400

    Return md_convert() HTML as glue

--- R/md-convert.R
+++ R/md-convert.R
@@ -1,14 +1,15 @@
 #' @title Convert Markdown to HTML
 #' @description Wraps around [markdown::markdownToHTML()] to convert markdown
-#'   text into HTML _fragments_.
-#' @param md The markdown text to be converted.
+#'   text into HTML _fragments_. Primarily used to test that `md_*()` functions
+#'   create vectors that can be properly rendered as HTML.
+#' @param x The _markdown_ text (from `md_*()`) to be converted.
 #' @param frag logical; Whether only a single HTML fragment should be returned.
-#' @return The HTML fragment equivalent.
+#'   `TRUE` by default.
+#' @return A `glue` vector of length 1 containing HTML tags.
 #' @examples
-#' md_convert(md = md_bold("Test"))
+#' md_convert(x = md_bold("test"))
 #' @importFrom glue glue
 #' @export
-md_convert <- function(md, frag = TRUE) {
-  html <- markdown::markdownToHTML(text = md, fragment.only = frag)
-  return(html)
+md_convert <- function(x, frag = TRUE) {
+  glue::as_glue(markdown::markdownToHTML(text = x, fragment.only = frag))
 }

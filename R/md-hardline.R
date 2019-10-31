58045e0970972353f60fcd77d86ad6022aa4e21e
Kiernan Nicholls <kiernann@protonmail.com>
2019-10-30 20:46:44 -0400

    Create md_softline()

--- R/md-hardline.R
+++ R/md-hardline.R
@@ -1,7 +1,8 @@
-#' Markdown hard line breaks (6.12)
+#' Markdown hard line breaks
 #'
-#' Separate strings with a hard line (non-paragraph) break. This inline hard
-#' line break can be rendered as `<br />` HTML tags.
+#' Take a character vector and return a collapsed glue vector with each original
+#' element separated by two spaces and a newline. This inline is rendered as a
+#' `<br />` HTML tag.
 #'
 #' @details
 #' A line break (not in a code span or HTML tag) that is preceded by two or more
@@ -12,10 +13,37 @@
 #'   spaces and a newline.
 #' @family inline functions
 #' @examples
+#' # compare the following
+#' md_bold(c("One", "Two"))
+#'
 #' md_hardline(md_bold(c("One", "Two")))
-#' md_hardline(state.name[1:10])
 #' @importFrom glue glue_collapse
 #' @export
 md_hardline <- function(x) {
   glue::glue_collapse(x, sep = "  \n")
+}
+
+#' Markdown soft line breaks
+#'
+#' Take a character vector and return a collapsed glue vector with each original
+#' element separated a single newline.
+#'
+#' @details
+#' A regular line break (not in a code span or HTML tag) that is not preceded by
+#' two or more spaces or a backslash is parsed as a softbreak. (A softbreak may
+#' be rendered in HTML either as a line ending or as a space. The result will be
+#' the same in browsers. In the examples here, a line ending will be used.)
+#' @param x A character vector of bullet point list items.
+#' @return A `glue` vector of length 1 with elements of `x` separated by two
+#'   spaces and a newline.
+#' @family inline functions
+#' @examples
+#' # compare the following
+#' md_bold(c("One", "Two"))
+#'
+#' md_softline(md_bold(c("One", "Two")))
+#' @importFrom glue glue_collapse
+#' @export
+md_softline <- function(x) {
+  glue::glue_collapse(x, sep = "\n")
 }

3202b3f6bed202b27152df5588c3abe21fe1ef47
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-03 12:13:50 -0500

    Test 6.5 strikethrough

---
+++ tests/testthat/test-6.5-strikethrough.R
@@ -0,0 +1,22 @@
+library(testthat)
+library(gluedown)
+library(rvest)
+library(glue)
+
+test_that("md_strike creates a <del> tag (ex. 491)", {
+  # https://github.github.com/gfm/#example-491
+  text <- "Hi Hello, world!"
+  target <- emphasize_at(text, md_strike, 1)
+  target %>%
+    md_convert() %>%
+    read_html() %>%
+    html_node("del") %>%
+    html_text() %>%
+    expect_equal("Hi")
+  target %>%
+    md_convert() %>%
+    read_html() %>%
+    html_node("p") %>%
+    html_text() %>%
+    expect_equal(text)
+})

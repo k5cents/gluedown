335c77c63cd97a679c98fdeb17ea7d1c99736865
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-03 13:11:08 -0500

    Test 6.8 autolinks

---
+++ tests/testthat/test-6.8-autolinks.R
@@ -0,0 +1,15 @@
+library(testthat)
+library(gluedown)
+library(rvest)
+library(glue)
+
+test_that("md_image creates a <img> tag (ex. 602)", {
+  # https://github.github.com/gfm/#example-602
+  url <- "http://foo.bar.baz"
+  md_autolink(url) %>%
+    md_convert() %>%
+    read_html() %>%
+    html_node("a") %>%
+    html_attr("href") %>%
+    expect_equal(url)
+})

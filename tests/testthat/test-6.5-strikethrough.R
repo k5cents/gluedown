44c207e0a73a579bb26335eb5d601a032872733a
Kiernan Nicholls <kiernann@protonmail.com>
2019-12-03 21:02:29 -0500

    Fix md_strike() test

--- tests/testthat/test-6.5-strikethrough.R
+++ tests/testthat/test-6.5-strikethrough.R
@@ -5,7 +5,7 @@ library(glue)
 
 test_that("md_strike creates a <del> tag (ex. 491)", {
   # https://github.github.com/gfm/#example-491
-  text <- "~~Hi~~ Hello, world!"
+  text <- paste(md_strike("Hi"), "Hello", "world")
   text %>%
     md_convert() %>%
     read_html() %>%
